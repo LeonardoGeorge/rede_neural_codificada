e84d684243f25e327efc51d1e764bd25aa0dbc28
Leonardo George <leonardogeorge30@gmail.com>
2025-09-14 15:48:49 -0300

    Update-api-Services

--- app/Services/WhisperService.php
+++ app/Services/WhisperService.php
@@ -15,6 +15,7 @@ class WhisperService
 
     public function __construct()
     {
+        // Use a configuração correta do cliente OpenAI
         $this->client = OpenAI::client(env('OPENAI_API_KEY'));
         $this->model = env('WHISPER_MODEL', 'whisper-1');
     }
@@ -35,7 +36,7 @@ class WhisperService
                 'model' => $this->model,
                 'file' => $audioFile,
                 'response_format' => 'json',
-                'language' => $language, // Opcional: forçar idioma português
+                'language' => $language, // Forçar idioma português
             ]);
 
             // Fechar o arquivo
@@ -48,11 +49,12 @@ class WhisperService
         }
     }
 
-    public function transcribeAudioFromBlob($audioBlob, $extension = 'webm')
+    // Método adicional para processar diretamente do request (opcional)
+    public function transcribeFromRequest($audioFile)
     {
         try {
-            // Salvar blob temporariamente
-            $tempFilename = 'temp_audio_' . time() . '.' . $extension;
+            // Salvar arquivo temporariamente
+            $tempFilename = 'temp_audio_' . time() . '.' . $audioFile->extension();
             $tempPath = storage_path('app/temp/' . $tempFilename);
 
             // Garantir que o diretório existe
@@ -61,7 +63,7 @@ class WhisperService
             }
 
             // Salvar arquivo temporário
-            file_put_contents($tempPath, $audioBlob);
+            $audioFile->move(storage_path('app/temp'), $tempFilename);
 
             // Transcrever
             $transcription = $this->transcribeAudio($tempPath);
@@ -71,7 +73,7 @@ class WhisperService
 
             return $transcription;
         } catch (\Exception $e) {
-            Log::error('Erro no processamento do blob de áudio: ' . $e->getMessage());
+            Log::error('Erro no processamento do áudio do request: ' . $e->getMessage());
             return false;
         }
     }
